47e2fb7196a22377670948fefb4098cf66a56bcf
1Dohyeon <dh1072005@gmail.com>
2024-03-25 18:44:44 +0900

    main -> frame

--- univ/app/src/main/java/example/univ/myapplication/MainActivity.kt
+++ univ/app/src/main/java/example/univ/myapplication/MainActivity.kt
@@ -6,29 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import example.univ.myapplication.databinding.FrameLayoutBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: FrameLayoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FrameLayoutBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.redButton.setOnClickListener {
-            binding.redView.visibility = View.VISIBLE
-            binding.greenView.visibility = View.INVISIBLE
-            binding.blueView.visibility = View.INVISIBLE
-        }
-
-        binding.greenButton.setOnClickListener {
-            binding.redView.visibility = View.INVISIBLE
-            binding.greenView.visibility = View.VISIBLE
-            binding.blueView.visibility = View.INVISIBLE
-        }
-
-        binding.blueButton.setOnClickListener {
-            binding.redView.visibility = View.INVISIBLE
-            binding.greenView.visibility = View.INVISIBLE
-            binding.blueView.visibility = View.VISIBLE
-        }
     }
 }
