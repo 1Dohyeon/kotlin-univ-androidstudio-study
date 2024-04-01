30929ba0ad99e3e1b3fd15e67a4e22289216c1bc
1Dohyeon <dh1072005@gmail.com>
2024-04-01 12:29:29 +0900

    0401 timer

--- univ/app/src/main/java/example/univ/myapplication/MainActivity.kt
+++ univ/app/src/main/java/example/univ/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package example.univ.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import example.univ.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.start.setOnClickListener { binding.timer.start() }
-        binding.stop.setOnClickListener { binding.timer.stop() }
+        val DEBUG_TAG = "MyActivity"
+
+        binding.timer.stop()
+
+        // start 를 안 눌러도 timer가 흐르긴 함. start는 보여주기만 할 뿐
+        binding.start.setOnClickListener {
+            binding.timer.start()
+            Log.d(DEBUG_TAG, "clicked_start")
+        }
+
+        // stop 도 마찬가지임. timer를 멈추지 않고, 보여주는 것만 멈춤
+        binding.stop.setOnClickListener {
+            binding.timer.stop()
+            Log.d(DEBUG_TAG, "clicked_stop")
+        }
     }
 }
