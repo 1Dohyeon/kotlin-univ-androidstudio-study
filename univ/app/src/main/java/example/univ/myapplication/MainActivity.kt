cdba4aba3649b07a2c88b2fb77ab12b2a2ae45f3
1Dohyeon <dh1072005@gmail.com>
2024-04-03 21:16:15 +0900

    0403

--- univ/app/src/main/java/example/univ/myapplication/MainActivity.kt
+++ univ/app/src/main/java/example/univ/myapplication/MainActivity.kt
@@ -3,10 +3,15 @@ package example.univ.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.widget.ArrayAdapter
+import android.widget.MultiAutoCompleteTextView
 import example.univ.myapplication.databinding.ActivityMainBinding
+import example.univ.myapplication.databinding.AutocompleteBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+     lateinit var binding2: AutocompleteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -25,6 +30,43 @@ class MainActivity : AppCompatActivity() {
         binding.stop.setOnClickListener {
             binding.timer.stop()
             Log.d(DEBUG_TAG, "clicked_stop")
+        }
+
+//        autocomplete
+//        binding2 = AutocompleteBinding.inflate(layoutInflater)
+//        setContentView(binding2.root)
+//        val countries = resources.getStringArray(R.array.countries_array)
+//        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
+//        binding2.auto.setAdapter(adapter)
+//        binding2.multiauto.threshold = 1
+//        binding2.multiauto.setTokenizer(MultiAutoCompleteTextView.CommaTokenizer())
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        var DEBUG_TAG: String = "MyActivity"
+        return when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                Log.d(DEBUG_TAG, "Action was DOWN")
+                true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                Log.d(DEBUG_TAG, "Action was MOVE")
+                true
+            }
+            MotionEvent.ACTION_UP -> {
+                Log.d(DEBUG_TAG, "Action was UP")
+                true
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                Log.d(DEBUG_TAG, "Action was CANCEL")
+                true
+            }
+            MotionEvent.ACTION_OUTSIDE -> {
+                Log.d(DEBUG_TAG, "Movement occured outside bounds of current screen element")
+                true
+            }
+            else -> super.onTouchEvent(event)
         }
     }
 }
