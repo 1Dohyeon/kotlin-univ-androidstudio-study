712c1eaf04967adb06914a11c72a36fa170d3e2c
1Dohyeon <dh1072005@gmail.com>
2024-04-16 23:21:15 +0900

    0416 adapter, viewpager 사용

--- univ/app/src/main/java/example/univ/myapplication/MainActivity.kt
+++ univ/app/src/main/java/example/univ/myapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import example.univ.myapplication.databinding.ActivityMainBinding
 import example.univ.myapplication.databinding.TablayoutBinding
 
@@ -23,30 +24,38 @@ class MainActivity : AppCompatActivity() {
         supportActionBar!!.setHomeAsUpIndicator(androidx.appcompat.R.drawable.abc_btn_colored_material)
         supportActionBar!!.setDisplayShowTitleEnabled(false)
 
-        /** Tab */
-        val tab = binding.tab
-        val tab1: TabLayout.Tab = tab.newTab()
-        tab1.text = "tab1"
-        tab.addTab(tab1)
-        tab.addTab(tab.newTab().setText("tab2"))
-        tab.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                val transaction = supportFragmentManager.beginTransaction()
-                when(tab?.text){
-                    "tab1" -> transaction.replace(R.id.tabContent, ExampleFragment())
-                    "tab2" -> transaction.replace(binding.tabContent.id, ExampleFragmentTwo())
-                }
-                transaction.commit()
-            }
+        /** Tab: adapter 없이 */
+//        val tab = binding.tab
+//        val tab1: TabLayout.Tab = tab.newTab()
+//        tab1.text = "tab1"
+//        tab.addTab(tab1)
+//        tab.addTab(tab.newTab().setText("tab2"))
+//        tab.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
+//                val transaction = supportFragmentManager.beginTransaction()
+//                when(tab?.text){
+//                    "tab1" -> transaction.replace(R.id.tabContent, ExampleFragment())
+//                    "tab2" -> transaction.replace(binding.tabContent.id, ExampleFragmentTwo())
+//                }
+//                transaction.commit()
+//            }
+//
+//            override fun onTabReselected(tab: TabLayout.Tab?) {
+//                /*   */
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {
+//                /*   */
+//            }
+//        })
 
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                /*   */
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                /*   */
-            }
-        })
+        /** Tab: viewpager와 adapter 활용 */
+        val tabLayout = binding.tab
+        val viewPage = binding.viewpager
+        viewPage.adapter = FragmentAdapter(supportFragmentManager, lifecycle)
+        TabLayoutMediator(tabLayout, viewPage) {tab, position ->
+            tab.text = "Tab $position"
+        }.attach()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
