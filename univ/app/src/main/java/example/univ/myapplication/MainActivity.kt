de76e22bed081e65513e3b451bd8e63a12a1093f
1Dohyeon <dh1072005@gmail.com>
2024-04-26 17:11:58 +0900

    0426

--- univ/app/src/main/java/example/univ/myapplication/MainActivity.kt
+++ univ/app/src/main/java/example/univ/myapplication/MainActivity.kt
@@ -10,7 +10,9 @@ import android.view.View
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.Chronometer
+import android.widget.FrameLayout
 import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.TimePicker
 import androidx.annotation.RequiresApi
@@ -30,24 +32,31 @@ class MainActivity : AppCompatActivity() {
 
         // 타이머 관련 binding
         val timer: Chronometer = binding.chrono
-        val startBookingBtn: Button = binding.btnStart
-        val endBookingBtn: Button = binding.btnEnd
 
         // 날짜, 시간 관련 binding
         val setDate: RadioButton = binding.rdoCal
         val setTime: RadioButton = binding.rdoTime
+        val radioGroup: RadioGroup = binding.radioGroup
         val calendarView: CalendarView = binding.calenderView
         val timePicker: TimePicker = binding.timerPicker
+        val dateGroup: FrameLayout = binding.dateViews
+        val textBar: TextView = binding.res
 
         var flag = false
         var time: Long = SystemClock.elapsedRealtime() - timer.base
 
-        // 예약 시작 버튼 이벤트
-        startBookingBtn.setOnClickListener {
+        val calenderView = findViewById<CalendarView>(R.id.calenderView)
+        val timerPicker = findViewById<TimePicker>(R.id.timerPicker)
+        val resTextView = findViewById<TextView>(R.id.res)
+
+        timer.setOnClickListener {
             if (!flag) { // 타이머가 비활성화 상태
                 timer.base = SystemClock.elapsedRealtime() - time
                 timer.start() // 타이머 시작
                 flag = true
+
+                radioGroup.visibility = View.VISIBLE
+                dateGroup.visibility = View.VISIBLE
 
                 Log.d(DEBUG_TAG, "Timer.base: ${timer.base}")
                 Log.d(DEBUG_TAG, "SystemClock.elapsedRealtime(): ${SystemClock.elapsedRealtime()}")
@@ -55,36 +64,39 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        val calenderView = findViewById<CalendarView>(R.id.calenderView)
-        val timerPicker = findViewById<TimePicker>(R.id.timerPicker)
-        val resTextView = findViewById<TextView>(R.id.res)
+        var selectedYear = 0
+        var selectedMonth = 0
+        var selectedDay = 0
 
-        // 예약 완료 버튼 이벤트
-        endBookingBtn.setOnClickListener {
-            if (flag) { // 타이머가 활성화 상태
+        calenderView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            selectedYear = year
+            selectedMonth = month + 1 // 월은 0부터 시작하므로 +1
+            selectedDay = dayOfMonth
+        }
+
+        textBar.setOnClickListener {
+
+            if(flag) {
                 timer.stop() // 타이머 정지
                 flag = false
                 time = SystemClock.elapsedRealtime() - timer.base
 
-                // 선택한 날짜 가져오기
-                val calendar = Calendar.getInstance()
-                calendar.timeInMillis = calenderView.date
-                val year = calendar.get(Calendar.YEAR)
-                val month = calendar.get(Calendar.MONTH) + 1 // 월은 0부터 시작하므로 +1
-                val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+                radioGroup.visibility = View.INVISIBLE
+                dateGroup.visibility = View.INVISIBLE
 
                 // 선택한 시간 가져오기
                 val hourOfDay = timerPicker.hour
                 val minute = timerPicker.minute
 
                 // TextView에 날짜 및 시간 설정
-                resTextView.text = String.format("%d년 %d월 %d일 %d시 %d분", year, month, dayOfMonth, hourOfDay, minute)
+                resTextView.text = String.format("%d년 %d월 %d일 %d시 %d분", selectedYear, selectedMonth, selectedDay, hourOfDay, minute)
 
                 Log.d(DEBUG_TAG, "Time: $time")
                 Log.d(DEBUG_TAG, "SystemClock.elapsedRealtime(): ${SystemClock.elapsedRealtime()}")
                 Log.d(DEBUG_TAG, "Timer.base: ${timer.base}")
             }
         }
+
 
         // RadioGroup 이벤트
         setDate.setOnClickListener {
