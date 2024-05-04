9e568b1659527e3af46e8542ac3c9038b245d78f
1Dohyeon <dh1072005@gmail.com>
2024-05-04 14:30:17 +0900

    0504

--- Week8/app/src/main/java/example/com/week8/MainActivity.kt
+++ Week8/app/src/main/java/example/com/week8/MainActivity.kt
@@ -1,11 +1,38 @@
 package example.com.week8
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import example.com.week8.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    fun readDiary(fName: String) : String?{
+        return null
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val context = applicationContext
+        val TAG = "Debug"
+
+        var cal = Calendar.getInstance()
+        var cy = cal.get(Calendar.YEAR)
+        var cm = cal.get(Calendar.MONTH)
+        var cd = cal.get(Calendar.DAY_OF_MONTH)
+        lateinit var fName: String
+
+        binding.datePciker.init(cy, cm, cd) {view, year, month, day ->
+
+        }
+        binding.smtBtn.setOnClickListener {
+            openFileOutput(fName, Context.MODE_PRIVATE).use {
+
+            }
+        }
     }
 }
