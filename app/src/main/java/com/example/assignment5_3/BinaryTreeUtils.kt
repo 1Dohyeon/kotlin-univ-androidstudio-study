e47843d2b1c11efa4d195336f09ce8f79abd8717
1Dohyeon <dh1072005@gmail.com>
2024-05-23 21:49:03 +0900

    add: 과제 5-3

---
+++ app/src/main/java/com/example/assignment5_3/BinaryTreeUtils.kt
@@ -0,0 +1,58 @@
+package com.example.assignment5_3
+
+object BinaryTreeUtils {
+
+    fun createBinaryTree(arr: Array<Int>): Node? {
+        if (arr.isEmpty()) return null
+        return insertLevelOrder(arr, null, 1)
+    }
+
+    private fun insertLevelOrder(arr: Array<Int>, root: Node?, i: Int): Node? {
+        var root = root
+        if (i < arr.size) {
+            val temp = Node(arr[i])
+            root = temp
+
+            // Adjusting indices to match 1-based array
+            val leftIndex = 2 * i
+            val rightIndex = 2 * i + 1
+
+            if (leftIndex < arr.size) {
+                root.left = insertLevelOrder(arr, root.left, leftIndex)
+            }
+            if (rightIndex < arr.size) {
+                root.right = insertLevelOrder(arr, root.right, rightIndex)
+            }
+        }
+        return root
+    }
+
+    fun heapify(arr: ArrayList<Int>) {
+        val n = arr.size
+        for (i in n / 2 - 1 downTo 1) {
+            heapifyDown(arr, n, i)
+        }
+    }
+
+    private fun heapifyDown(arr: ArrayList<Int>, n: Int, i: Int) {
+        var largest = i
+        val left = 2 * i
+        val right = 2 * i + 1
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right
+        }
+
+        if (largest != i) {
+            val swap = arr[i]
+            arr[i] = arr[largest]
+            arr[largest] = swap
+
+            heapifyDown(arr, n, largest)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/assignment5_3/Node.kt
@@ -0,0 +1,6 @@
+package com.example.assignment5_3
+
+class Node(val value: Int) {
+    var left: Node? = null
+    var right: Node? = null
+}
